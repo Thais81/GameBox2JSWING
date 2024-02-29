e78f22b563d453dc58abf4efffa351842c894568
Thaïs Genin <genin.thais@gmail.com>
2024-02-29 16:38:39 +0100

    Popup fonctionnelle avec commentaire et JavaDoc

--- src/main/java/graphics/Popup.java
+++ src/main/java/graphics/Popup.java
@@ -13,12 +13,23 @@ import javax.swing.JOptionPane;
  */
 public class Popup {
 
-    private ImageIcon[] icons = {new ImageIcon("win.png"), new ImageIcon("lose.png")};
+    /**
+     * @Class Popup Fenêtre Popup qui s'affichera pendant une gestion
+     * evènementielle. En     * fonction de la réponse donnée, s'affichera avec les index des tableau
+     * pour vrai ou faux.
+     */
 
+    private ImageIcon[] icons = {new ImageIcon("win.png"), new ImageIcon("lose.png")};
     private String[] titles = {"Gagné!", "Perdu!"};
     private String[] messages = {"Bonne réponse!", "C'est perdu gros looser, booooouuuuh!"};
 
     public void showPopup(int i) {
+        /**
+         * @showPopup         * i correspond à l'indice des différents tableaux. Comme il y a peu de
+         * valeurs par tableau (seulement deux), choix d'ordonnacer
+         * directement         * les valeurs correspondantes aux mêmes indices au lieux de faire des
+         * tableaux de valeurs.
+         */
         ImageIcon icon = icons[i];
         String message = messages[i];
         String title = titles[i];
