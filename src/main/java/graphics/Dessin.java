a9b3a251dc1ef40218e43cabbede6d86d236722b
Jopaups <paupardin.jo@gmail.com>
2024-02-29 16:32:15 +0100

    AddMouseListener fonctionnel! L'ardoise magique peut être utilisée Resteà configurer le bouton delete et le choix de couleur

--- src/main/java/graphics/Dessin.java
+++ src/main/java/graphics/Dessin.java
@@ -1,24 +1,34 @@
 package graphics;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.plaf.basic.BasicComboBoxRenderer;
 
 public class Dessin extends JPanel {
-    
+
     JPanel drawPanel, toolPanel;
     JButton delete;
     JLabel colorLabel;
     JLabel titleJLabel;
-    JComboBox<Color> colorBox;
-    
+    JComboBox<String> colorBox;
+    int x, y = 0;
+
     public Dessin() {
         initGui();
+        initEvents();
     }
 
     private void initGui() {
@@ -35,8 +45,8 @@ public class Dessin extends JPanel {
         drawPanel.setPreferredSize(new Dimension(300, 300));
         drawPanel.setBackground(Color.WHITE);
         this.add(drawPanel);
-        
-        Color[] options = {Color.RED, Color.BLUE, Color.GREEN};
+
+        String[] options = {"Rouge", "Bleu", "Vert"};
         colorBox = new JComboBox<>(options);
 
         // Configurez le panel d'outils
@@ -44,8 +54,64 @@ public class Dessin extends JPanel {
         toolPanel.add(delete);
         colorLabel.setText("Color :");
         toolPanel.add(colorLabel);
+        colorBox.setRenderer(new ColorBoxRenderer());
         toolPanel.add(colorBox);
 
         this.add(toolPanel);
+    }
+
+    private void initEvents() {
+        drawPanel.addMouseMotionListener(new MouseMotionListener() {
+
+            @Override
+            public void mouseMoved(MouseEvent me) {
+                x = me.getX();
+                y = me.getY();
+            }
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                Graphics g = drawPanel.getGraphics();
+                g.drawLine(x, y, e.getX(), e.getY());
+                x = e.getX();
+                y = e.getY();
+            }
+        });
+    }
+
+    private class ColorBoxRenderer extends BasicComboBoxRenderer {
+
+        @Override
+        public Component getListCellRendererComponent(JList<?> list, Object value, int index,
+                boolean isSelected, boolean cellHasFocus) {
+            Component renderer = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+
+            if (value != null) {
+                String text = value.toString();
+                Color color = getColorFromText(text);
+                renderer.setBackground(color);
+                renderer.setForeground(getContrastColor(color));
+            }
+
+            return renderer;
+        }
+
+        private Color getColorFromText(String text) {
+            switch (text) {
+                case "Rouge":
+                    return Color.RED;
+                case "Bleu":
+                    return Color.BLUE;
+                case "Vert":
+                    return Color.GREEN;
+                default:
+                    return Color.BLACK;
+            }
+        }
+
+        private Color getContrastColor(Color color) {
+            int luminance = (int) (0.299 * color.getRed() + 0.587 * color.getGreen() + 0.114 * color.getBlue());
+            return luminance > 128 ? Color.BLACK : Color.WHITE;
+        }
     }
 }
